5252c28ef998f8bfe458b411b55c85c8c0b60cd8
Smith Ringtho <sringtho@gmail.com>
2023-05-03 10:57:37 +0000

    Added Todo component structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import Navbar from './components/Navbar';
+import Todo from "./components/Todo"
 
 function App() {
   return (
     <div className="app">
       <Navbar />
-      <h2>Smith Ringtho</h2>
+      <section className="tasks-container">
+        <Todo />
+      </section>
 
     </div>
   );
---
+++ src/components/Todo.tsx
@@ -0,0 +1,12 @@
+
+
+const Todo = () => {
+    return (
+        <div className="todo-header">
+            <div className="todo-circle"></div>
+            <p className="todo-title">Todo (4)</p>
+        </div>
+    )
+}
+
+export default Todo
